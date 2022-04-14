31da25e4b3ed3a08fed7e81dcd735a0be5c8e39b
apimento <98665437+apimento@users.noreply.github.com>
2022-04-14 13:18:22 -0400

    pokemon lab completed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ import React, { Component } from 'react'
 import Pokemon from './components/Pokemon/Pokemon';
 
 export default class App extends Component {
-  render() {
+
+state = { 
+      hp1: 120, 
+      hp2: 100
+    } 
+
+  handlePikachuAttack = () => {
+    this.setState({hp1: this.state.hp1 - 10})
+    }
+
+    handleCharizardAttack = () => {
+    this.setState({hp2: this.state.hp2 - 10})
+    }
+
+
+  render(){
     return (
-      <div className="App">App</div>
+      <div className="App"> 
+      <Pokemon name={"Charizard"} hp={this.state.hp1} image={"https://pokestop.io/img/pokemon/charizard-256x256.png"} handleAttack={this.handleCharizardAttack}>  </Pokemon> 
+      <Pokemon name={"Pikachu"} hp={this.state.hp2} image= {"https://pokestop.io/img/pokemon/pikachu-256x256.png"} handleAttack={this.handlePikachuAttack}> </Pokemon>
+      </div>
     )
   }
 }
-
--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Pokemon(props) {
   return (
-    <div className="Pokemon">Pokemon</div>
+    <div className="Pokemon">
+      <div> {props.name} {props.hp} </div> 
+      <img src={props.image} alt=""/> 
+      <button onClick={props.handleAttack}>Attack!</button>
+
+
+    </div>
   )
 }
